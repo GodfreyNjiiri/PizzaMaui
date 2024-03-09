c32175b5919cd495dc03594f7f65a814b78cbab4
GodfreyNjiiri <81634694+GodfreyNjiiri@users.noreply.github.com>
2024-03-09 20:08:48 +0300

    Add project files.

---
+++ PizzaMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
